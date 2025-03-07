eeb350b4d0f0db058079ba89c5b0b540f68046cd
RaufAbasbayli041 <abasbeyli97@gmail.com>
2025-03-08 00:46:36 +0400

    PB503 JS-Fetch-2 rev1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,42 @@
-const BASE_URL = "https://restcountries.com/v3.1/all ";
-const cards = document.querySelector(".card-wrapper");
+const BASE_URL = "https://restcountries.com/v3.1/all";
 
-
-
-fetch(BASE_URL)
-  .then((response) => response.json())
-  .then((datas) => {
-    datas.forEach((data) => {
-      cards.innerHTML += ` <div class="card" >
-            <img src="${data.flags.svg}" class="card-img-top flags" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">${data.name.common}</h5>
-            </div>
-            <ul class="list-group list-group-flush " >
-              <li class="list-group-item">Population: ${data.population}</li>
-              <li class="list-group-item">Region: ${data.region}</li>
-              <li class="list-group-item">Capital: ${data.capital}</li>
-            </ul>
-          </div>`;
+function getAllDatas() {
+  fetch(BASE_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      printCards(data);
     });
+}
+
+getAllDatas();
+
+function printCards(countries) {
+  const cards = document.querySelector(".cards");
+  countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
+  countries.forEach((country) => {
+    const cardWrapper = document.createElement("div");
+    cardWrapper.className =
+      " col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center divcl";
+    cardWrapper.innerHTML = ` 
+          <div class="card ">
+          <a href="" >
+                  <div class="image-wrapper">
+                  <img src="${country.flags.svg}" class="card-img-top flag" alt="..." />
+                    </div>
+                    <div class="card-body">
+                    <h5 class="card-title">${country.name.common}</h5>
+                    </div>
+                    <ul class="list-group list-group-flush desc" >
+                    <li class="list-group-item">Population: ${country.population}</li>
+                    <li class="list-group-item">Region: ${country.region}</li>
+                    <li class="list-group-item">Capital: ${country.capital}</li>
+                    </ul>
+                    </div>
+                    </div> 
+                    </a>
+                    </div>`;
+
+    cards.appendChild(cardWrapper);
   });
+}
